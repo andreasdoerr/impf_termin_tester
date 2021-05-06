5266864c8b0f01bce0a1bd71e732ce0c6b2f3d86
Bastian <44191521+bastian987@users.noreply.github.com>
2021-05-06 21:44:49 +0200

    added mqtt as notification option (#15)
    Co-authored-by: Andreas Doerr <andreasdoerr@gmx.net>

--- example.py
+++ example.py
@@ -36,6 +36,9 @@ if __name__ == "__main__":
 
     # Output directory for file dump
     output_dir = "output_folder_path"
+    
+    # MQTT Broker
+    broker = "address_of_your_mqtt_broker"
 
     # Create notifiers
     notifiers = [
@@ -44,6 +47,7 @@ if __name__ == "__main__":
         # PushSaferNotification(pushsafer_private_key),
         # OutlookNotification(email),    # Windows only, requiers pywin32
         FileNotification(output_dir),
+        # MQTTNotification(broker)
     ]
 
     # Communication with browser
---
+++ impf_termin_tester/notifiers/mqtt.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+from impf_termin_tester.notifiers import NotificationService
+
+
+class MQTTNotification(NotificationService):
+    def __init__(self, broker):
+        super().__init__(name="MQTT client")
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_topic = "impf_termin"
+        self.mqtt_broker = broker
+
+    def initialize(self):
+        self.mqtt_client.connect(self.mqtt_broker, port=1883, keepalive=60, bind_address="")
+
+    def _send_notification(self, result):
+        mqtt_payload = "Appointment available: " + result.url
+        ret = self.mqtt_client.publish(self.mqtt_topic, mqtt_payload, qos=0, retain=False)
+        return True
